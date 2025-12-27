fada6fd95f29eef55581061fb9cc2ea6d3e286a8
sandeshlad300 <sandeshlad300@gmail.com>
2025-12-27 11:03:30 +0530

    changes in gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 android {
     namespace = "com.sandesh.fintrack"
     compileSdk {
-        version = release(36)
+        version = release(34)
     }
 
     defaultConfig {
         applicationId = "com.sandesh.fintrack"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
