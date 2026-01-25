53ef33f692d7ab0185179d90f87bae26826571da
sandeshlad300 <sandeshlad300@gmail.com>
2026-01-25 18:39:02 +0530

    Upload updated build through firebase using drone.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sandesh.fintrack"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
